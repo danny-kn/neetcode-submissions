267b01e5cf2ca0aa214201be0656e8bf1b44b9e9
Danny Kan <danny_kn@proton.me>
2026-04-19 19:37:10 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,56 @@
+// import java.util.Map;
+// import java.util.HashMap;
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         Map<Character, Integer> firstMap = new HashMap<>();
+//         Map<Character, Integer> secondMap = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (firstMap.containsKey(s.charAt(i))) {
+//                 firstMap.put(s.charAt(i), firstMap.get(s.charAt(i)) + 1);
+//             } else {
+//                 firstMap.put(s.charAt(i), 1);
+//             }
+//         }
+//         for (int i = 0; i < t.length(); i++) {
+//             if (secondMap.containsKey(t.charAt(i))) {
+//                 secondMap.put(t.charAt(i), secondMap.get(t.charAt(i)) + 1);
+//             } else {
+//                 secondMap.put(t.charAt(i), 1);
+//             }
+//         }
+//         return firstMap.equals(secondMap);
+//     }
+// }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) { return false; }
+//         Map<Character, Integer> firstMap = populateMap(s);
+//         Map<Character, Integer> secondMap = populateMap(t);
+//         return firstMap.equals(secondMap);
+//     }
+
+//     public Map<Character, Integer> populateMap(String str) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         for (int i = 0; i < str.length(); i++) {
+//             if (map.containsKey(str.charAt(i))) {
+//                 map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+//             } else {
+//                 map.put(str.charAt(i), 1);
+//             }
+//         }
+//         return map;
+//     }
+// }
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) { return false; }
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
+}
