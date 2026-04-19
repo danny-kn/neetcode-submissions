7352dca9544afa3f05a985600aea6a9881cf8a48
Danny Kan <danny_kn@proton.me>
2026-04-19 10:28:53 -0400

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.java
@@ -0,0 +1,49 @@
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int[] ans = new int[2 * nums.length];
+//         for (int i = 0; i < nums.length; i++) {
+//             ans[i] = nums[i];
+//             ans[i + nums.length] = nums[i];
+//         }
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int n = nums.length;
+//         int[] ans = new int[2 * n];
+//         for (int i = 0; i < n; i++) {
+//             ans[i] = nums[i];
+//             ans[i + n] = nums[i];
+//         }
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int[] ans = new int[2 * nums.length];
+//         int i = 0;
+//         while (i < nums.length) {
+//             ans[i] = nums[i];
+//             ans[i + nums.length] = nums[i];
+//             i++;
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        int i = 0;
+        while (i < n) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+            i++;
+        }
+        return ans;
+    }
+}
