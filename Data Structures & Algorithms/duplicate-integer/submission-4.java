1f35bfc360ae4b430db774f9ea2220212e5066ce
Danny Kan <danny_kn@proton.me>
2026-04-19 10:48:42 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,74 @@
+// import java.util.Set;
+// import java.util.HashSet;
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             set.add(nums[i]);
+//         }
+//         return set.size() != nums.length;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (set.contains(nums[i])) {
+//                 return true;
+//             }
+//             set.add(nums[i]);
+//         }
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+//         return set.size() != nums.length;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             if (set.contains(num)) {
+//                 return true;
+//             }
+//             set.add(num);
+//         }
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        boolean containsDuplicate = false;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                containsDuplicate = true;
+                return containsDuplicate;
+            }
+        }
+        return containsDuplicate;
+    }
+}
