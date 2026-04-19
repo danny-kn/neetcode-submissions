a1db17dfd21d09e7cc69cb3255c54916351ecca6
Danny Kan <danny_kn@proton.me>
2026-04-19 10:37:56 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,48 @@
+// import java.util.Set;
+// import java.util.HashSet;
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             set.add(nums[i]);
+//         }
+//         return set.size() != nums.length;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (set.contains(nums[i])) {
+//                 return true;
+//             }
+//             set.add(nums[i]);
+//         }
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             set.add(num);
+//         }
+//         return set.size() != nums.length;
+//     }
+// }
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
