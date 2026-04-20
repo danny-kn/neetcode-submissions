b466e4e9b145488ff4804386791110113c8e68a4
Danny Kan <danny_kn@proton.me>
2026-04-19 23:31:51 -0400

    Add: reverse-string - submission-4

---
+++ Data Structures & Algorithms/reverse-string/submission-4.java
@@ -0,0 +1,73 @@
+// import java.util.List;
+// import java.util.ArrayList;
+
+// import java.util.Stack;
+
+// import java.util.StringBuilder;
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         List<Character> list = new ArrayList<>();
+//         for (int i = 0; i < s.length; i++) {
+//             list.add(s[i]);
+//         }
+//         Collections.reverse(list);
+//         for (int i = 0; i < s.length; i++) {
+//             s[i] = list.get(i);
+//         }
+//     }
+// }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         List<Character> list = new ArrayList<>();
+//         for (char ch : s) {
+//             list.add(ch);
+//         }
+//         Collections.reverse(list);
+//         for (int i = 0; i < s.length; i++) {
+//             s[i] = list.get(i);
+//         }
+//     }
+// }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (int i = 0; i < s.length; i++) {
+//             stack.push(s[i]);
+//         }
+//         for (int i = 0; i < s.length; i++) {
+//             s[i] = stack.pop();
+//         }
+//     }
+// }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (char ch : s) {
+//             stack.push(ch);
+//         }
+//         for (int i = 0; i < s.length; i++) {
+//             s[i] = stack.pop();
+//         }
+//     }
+// }
+
+class Solution {
+    public void reverseString(char[] s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length; i++) {
+            sb.append(s[i]);
+        }
+        sb.reverse().toString();
+        for (int i = 0; i < s.length; i++) {
+            s[i] = sb.charAt(i);
+        }
+    }
+}
+
+
+
+
