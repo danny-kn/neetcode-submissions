7956737abb48adf68ed5a0a117a5b61e5519a415
Danny Kan <danny_kn@proton.me>
2026-04-19 21:33:32 -0400

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// import java.util.PriorityQueue;
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+//         ListNode currNode = list1;
+//         while (currNode != null) {
+//             minHeap.add(currNode.val);
+//             currNode = currNode.next;
+//         }
+//         currNode = list2;
+//         while (currNode != null) {
+//             minHeap.add(currNode.val);
+//             currNode = currNode.next;
+//         }
+//         if (minHeap.isEmpty()) { return null; }
+//         ListNode newHead = new ListNode(minHeap.remove());
+//         currNode = newHead;
+//         while (!minHeap.isEmpty()) {
+//             currNode.next = new ListNode(minHeap.remove());
+//             currNode = currNode.next;
+//         }
+//         currNode.next = null;
+//         return newHead;
+//     }
+// }
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap = populateMinHeap(minHeap, list1);
+        minHeap = populateMinHeap(minHeap, list2);
+        if (minHeap.isEmpty()) { return null; }
+        ListNode newHead = new ListNode(minHeap.remove());
+        ListNode currNode = newHead;
+        while (!minHeap.isEmpty()) {
+            currNode.next = new ListNode(minHeap.remove());
+            currNode = currNode.next;
+        }
+        currNode.next = null;
+        return newHead;
+    }
+
+    public PriorityQueue<Integer> populateMinHeap(PriorityQueue<Integer> minHeap, ListNode list) {
+        ListNode currNode = list;
+        while (currNode != null) {
+            minHeap.add(currNode.val);
+            currNode = currNode.next;
+        }
+        return minHeap;
+    }
+}
