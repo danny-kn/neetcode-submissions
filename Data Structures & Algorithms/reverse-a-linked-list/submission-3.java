2f55c1887dbebc50f968303ecc4d9d31932630e6
Danny Kan <danny_kn@proton.me>
2026-04-19 21:45:04 -0400

    Add: reverse-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-3.java
@@ -0,0 +1,90 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// import java.util.Stack;
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         Stack<ListNode> stack = new Stack<>();
+//         ListNode currNode = head;
+//         while (currNode != null) {
+//             stack.push(currNode);
+//             currNode = currNode.next;
+//         }
+//         if (stack.isEmpty()) { return null; }
+//         ListNode newHead = stack.pop();
+//         currNode = newHead;
+//         while (!stack.isEmpty()) {
+//             currNode.next = stack.pop();
+//             currNode = currNode.next;
+//         }
+//         currNode.next = null;
+//         return newHead;
+//     }
+// }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        populateStack(stack, head);
+        if (stack.isEmpty()) { return null; }
+        ListNode newHead = stack.pop();
+        ListNode currNode = newHead;
+        while (!stack.isEmpty()) {
+            currNode.next = stack.pop();
+            currNode = currNode.next;
+        }
+        currNode.next = null;
+        return newHead;
+    }
+
+    public void populateStack(Stack stack, ListNode node) {
+        ListNode currNode = node;
+        while (currNode != null) {
+            stack.push(currNode);
+            currNode = currNode.next;
+        }
+    }
+}
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         Stack<Integer> stack = new Stack<>();
+//         ListNode currNode = head;
+//         while (currNode != null) {
+//             stack.push(currNode.val);
+//             currNode = currNode.next;
+//         }
+//         if (stack.isEmpty()) { return null; }
+//         ListNode newHead = new ListNode(stack.pop());
+//         currNode = newHead;
+//         while (!stack.isEmpty()) {
+//             currNode.next = new ListNode(stack.pop());
+//             currNode = currNode.next;
+//         }
+//         currNode.next = null;
+//         return newHead;
+//     }
+// }
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         ListNode prevNode = null;
+//         ListNode currNode = head;
+//         while (currNode != null) {
+//             ListNode tmpNode = currNode.next;
+//             currNode.next = prevNode;
+//             prevNode = currNode;
+//             currNode = tmpNode;
+//         }
+//         return prevNode;
+//     }
+// }
