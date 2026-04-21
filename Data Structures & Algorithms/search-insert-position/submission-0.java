d3d59b72313c35f13507708b03d3740d26d32600
Danny Kan <danny_kn@proton.me>
2026-04-20 21:55:17 -0400

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int a = 0;
+        int b = nums.length - 1;
+        while (a <= b) {
+            int m = a + ((b - a) / 2);
+            if (nums[m] < target) {
+                a = m + 1;
+            } else if (nums[m] > target) {
+                b = m - 1;
+            } else {
+                return m;
+            }
+        }
+        return a;
+    }
+}
