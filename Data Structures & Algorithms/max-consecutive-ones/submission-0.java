f5affa9d138fa42614b3ff1a2600dcb79914f9f5
Danny Kan <danny_kn@proton.me>
2026-04-23 10:52:43 -0400

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.java
@@ -0,0 +1,18 @@
+// import java.util.Map;
+// import java.util.HashMap;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveOnes = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxConsecutiveOnes = Math.max(maxConsecutiveOnes, count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxConsecutiveOnes;
+    }
+}
